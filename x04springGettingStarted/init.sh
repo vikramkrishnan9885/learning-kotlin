9bc63daed2b3be2670701428d34000ca9abb4993
Vikram Krishnan <kvikram9885@gmail.com>
2020-01-28 21:31:25 +0800

    Changed spring boot version, removed dependencies that were resulting in failing tests because of unused deps

--- x04springGettingStarted/init.sh
+++ x04springGettingStarted/init.sh
@@ -4,24 +4,32 @@ if [ $1 -eq 0 ]
 then
   rm -rf .gitignore
   rm -rf .mvn
-  rm HELP.md
+  rm -rf .gradle
+  rm -rf .idea
   rm build.gradle
+  rm build.gradle.kts
+  rm HELP.md
   rm -rf gradle
   rm gradlew
   rm gradlew.bat
   rm settings.gradle
+  rm settings.gradle.kts
   rm spring-getting-started.zip
   rm -rf src
+  rm -rf build
 fi
 
 spring init --build=gradle \
   --java-version=1.8 \
-  --boot-version=2.0.0.RELEASE \
+  --boot-version=2.2.4.RELEASE \
   --language=kotlin \
-  --dependencies=web,security,data-jpa,actuator,devtools \
   -g=com.vk.spring \
   -a=spring-getting-started \
   --package-name=com.vk.spring \
-  -name=spring-getting-started
+  -name=spring-getting-started \
+  #--dependencies=web,security,data-jpa,actuator,devtools \
 
 unzip spring-getting-started.zip
+
+.gradlew clean
+.gradlew build
---
+++ x04springGettingStarted/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar build/libs/spring-getting-started-0.0.1-SNAPSHOT.jar
