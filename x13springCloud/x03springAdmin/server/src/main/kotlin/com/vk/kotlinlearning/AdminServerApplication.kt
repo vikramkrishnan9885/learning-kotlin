8a0101db6183b62337fe1f2a286053e2a3beec1f
Vikram Krishnan <kvikram9885@gmail.com>
2020-02-08 18:06:46 +0800

    Basic working instance of Spring Admin

--- x13springCloud/x03springAdmin/server/src/main/kotlin/com/vk/kotlinlearning/AdminServerApplication.kt
+++ x13springCloud/x03springAdmin/server/src/main/kotlin/com/vk/kotlinlearning/AdminServerApplication.kt
@@ -1,8 +1,15 @@
 package com.vk.kotlinlearning
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
 
+// https://codecentric.github.io/spring-boot-admin/2.1.6/
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 @SpringBootApplication
 class AdminServerApplication
 
