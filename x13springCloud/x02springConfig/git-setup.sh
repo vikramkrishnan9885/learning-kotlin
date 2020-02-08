ae0cf13a793c0b64571fa525baebece41108ed76
Vikram Krishnan <kvikram9885@gmail.com>
2020-02-08 10:36:23 +0800

    Spring Cloud server

--- x13springCloud/x02springConfig/git-setup.sh
+++ x13springCloud/x02springConfig/git-setup.sh
@@ -5,6 +5,11 @@ cd git/cloud
 
 git init
 
+#
+# SUPER IMP
+# spring.application.name - Client should have this in its application.properties
+# Properties file name should be {spring.application.name}-{environment}.properties
+#
 echo '''
 limits-service.minimum=1
 limits-service.maximum=11
