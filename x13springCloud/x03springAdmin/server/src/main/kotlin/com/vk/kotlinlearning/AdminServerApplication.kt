e2a542482a3fc9efee93ac6e9b7e61465ab68fc2
Vikram Krishnan <kvikram9885@gmail.com>
2020-02-08 18:55:24 +0800

    Spring Boot Admin using Eureka client - PREFERRED METHOD

--- x13springCloud/x03springAdmin/server/src/main/kotlin/com/vk/kotlinlearning/AdminServerApplication.kt
+++ x13springCloud/x03springAdmin/server/src/main/kotlin/com/vk/kotlinlearning/AdminServerApplication.kt
@@ -4,11 +4,14 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.Configuration
 
 // https://codecentric.github.io/spring-boot-admin/2.1.6/
+// https://codecentric.github.io/spring-boot-admin/2.1.6/
 @Configuration
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 @EnableAdminServer
 @SpringBootApplication
 class AdminServerApplication
