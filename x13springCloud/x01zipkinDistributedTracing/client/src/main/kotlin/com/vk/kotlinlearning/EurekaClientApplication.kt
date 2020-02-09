3a67be56e1e5e6c1240f730d8be19911874350fa
Vikram Krishnan <kvikram9885@gmail.com>
2020-02-09 15:10:25 +0800

    Using Sleuth with Zipkin

--- x13springCloud/x01zipkinDistributedTracing/client/src/main/kotlin/com/vk/kotlinlearning/EurekaClientApplication.kt
+++ x13springCloud/x01zipkinDistributedTracing/client/src/main/kotlin/com/vk/kotlinlearning/EurekaClientApplication.kt
@@ -3,10 +3,13 @@ package com.vk.kotlinlearning
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import java.util.logging.Logger
 
 @EnableDiscoveryClient
 @SpringBootApplication
-class EurekaClientApplication
+class EurekaClientApplication{
+	private val LOG: Logger = Logger.getLogger(EurekaClientApplication::class.simpleName)
+}
 
 fun main(args: Array<String>) {
 	runApplication<EurekaClientApplication>(*args)
--- x13springCloud/x01zipkinDistributedTracing/client/src/main/kotlin/com/vk/kotlinlearning/HelloWorldController.kt
+++ x13springCloud/x01zipkinDistributedTracing/client/src/main/kotlin/com/vk/kotlinlearning/HelloWorldController.kt
@@ -5,12 +5,18 @@ package com.vk.kotlinlearning
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.util.logging.Level
+import java.util.logging.Logger
 
 
 @RestController
 class HelloWorldController {
+
+    private val LOG: Logger = Logger.getLogger(HelloWorldController::class.simpleName)
+
     @GetMapping("/hello-worlds/{name}")
     fun getHelloWorld(@PathVariable name: String): String {
+        LOG.log(Level.INFO, "This is a sample log from the controller")
         return "Hello World $name"
     }
 }
