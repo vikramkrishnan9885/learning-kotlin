f4aafde62f15b82372e16bf9ee46af253f628b31
Vikram Krishnan <kvikram9885@gmail.com>
2020-02-09 17:15:35 +0800

    Basics of Hystrix

--- x13springCloud/x05hystrix/x00basics/src/main/kotlin/com/vk/kotlinlearning/HystrixServerApplication.kt
+++ x13springCloud/x05hystrix/x00basics/src/main/kotlin/com/vk/kotlinlearning/HystrixServerApplication.kt
@@ -1,11 +1,55 @@
 package com.vk.kotlinlearning
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.logging.Level
+import java.util.logging.Logger
 
+@RestController
+@EnableHystrix
 @SpringBootApplication
-class HystrixServerApplication
+class HystrixServerApplication {
 
-fun main(args: Array<String>) {
-	runApplication<HystrixServerApplication>(*args)
+	companion object {
+
+		@JvmStatic
+		val LOG:Logger = Logger.getLogger(HystrixServerApplication::class.java.name)
+
+		@JvmStatic
+		fun main(args: Array<String>) {
+			runApplication<HystrixServerApplication>(*args)
+		}
+
+	}
+
+	@Throws(InterruptedException::class)
+	@RequestMapping("/")
+	@HystrixCommand(
+			fallbackMethod = "helloFallback"
+			, commandProperties = arrayOf(
+				HystrixProperty(
+						name = "execution.isolation.thread.timeoutInMilliseconds"
+						, value = "6000"
+				)
+			)
+	)
+	fun hello():String {
+		val max = 10
+		val min = 0
+		val timeSleep = ((Math.random()*(max -min + 1) + min)*1000).toLong()
+		LOG.log(Level.INFO,timeSleep.toString())
+		// Log before thread sleep otherwise it won't print because fallbackMethod gets invoked
+		Thread.sleep(timeSleep)
+
+		return "Welcome Hystrix"
+	}
+
+	fun helloFallback(): String {
+		return "Request fails. It takes long time to response"
+	}
 }
