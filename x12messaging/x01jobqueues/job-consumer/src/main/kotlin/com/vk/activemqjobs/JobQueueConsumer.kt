2390ed9f61735ebb8250fe3f575ef73e231bfb63
Vikram Krishnan <kvikram9885@gmail.com>
2020-03-13 21:47:57 +0800

    Added consumer main funcition - identical to producer

--- x12messaging/x01jobqueues/job-consumer/src/main/kotlin/com/vk/activemqjobs/JobQueueConsumer.kt
+++ x12messaging/x01jobqueues/job-consumer/src/main/kotlin/com/vk/activemqjobs/JobQueueConsumer.kt
@@ -8,12 +8,14 @@ import javax.jms.Session
 
 class JobQueueConsumer {
 
+    // IDENTICAL TO PRODUCER
     private var connectionFactory: ActiveMQConnectionFactory? = null
     private val connectionUri:String = "tcp://localhost:61616"
     private var connection: Connection? = null
     private var destination: Destination? = null
     private var session: Session? = null
 
+    // IDENTICAL TO PRODUCER
     @Throws(Exception::class)
     fun before(): Unit {
         connectionFactory = ActiveMQConnectionFactory(connectionUri)
@@ -22,6 +24,7 @@ class JobQueueConsumer {
         destination = session?.createQueue("JOBQ.Work")
     }
 
+    // IDENTICAL TO PRODUCER
     @Throws(Exception::class)
     fun after(): Unit {
         if (connection != null) {
@@ -31,6 +34,28 @@ class JobQueueConsumer {
 
     @Throws(Exception::class)
     fun run(): Unit {
+        //NOTE THAT THIS LINE IS A MIRROR IMAGE OF PRODUCER
         val consumer: MessageConsumer? = session?.createConsumer(destination)
+
+        // THIS BIT IS DIFFERENT
+    }
+
+    //NOTE THAT THIS IS A MIRROR IMAGE OF PRODUCER
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val consumer = JobQueueConsumer()
+            print("\n\n\n")
+            println("Starting example Job Queue Producer now...")
+            try {
+                consumer.before()
+                consumer.run()
+                consumer.after()
+            } catch (e: java.lang.Exception) {
+                println("Caught an exception during the example: " + e.message)
+            }
+            println("Finished running the sample Job Queue Producer app.")
+            print("\n\n\n")
+        }
     }
 }
