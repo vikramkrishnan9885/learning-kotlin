c62baea93f30756afa57790c231d3c42b99bd0c4
Vikram Krishnan <kvikram9885@gmail.com>
2020-03-16 08:21:28 +0800

    Fixing build.gradle and adding createProducer function

---
+++ x12messaging/x01kafka/x00producer/src/main/kotlin/com/vk/kafkakotlin/SimpleProducer.kt
@@ -0,0 +1,19 @@
+package com.vk.kafkakotlin
+
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.common.serialization.StringSerializer
+import java.util.*
+
+class SimpleProducer(brokers: String) {
+
+    private fun createProducer(brokers: String): Producer<String, String> {
+        val props = Properties()
+        props["bootstrap.servers"] = brokers
+        props["key.serializer"] = StringSerializer::class.java
+        props["value.serializer"] = StringSerializer::class.java
+
+        return KafkaProducer<String, String>(props)
+    }
+
+}
