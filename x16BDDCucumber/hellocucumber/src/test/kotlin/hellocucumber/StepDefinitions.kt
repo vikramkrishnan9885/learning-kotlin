ab2d22c43ff067dddc97f56b495c4d696f7858de
Vikram Krishnan <kvikram9885@gmail.com>
2020-04-29 20:35:34 +0800

    Passing tests

--- x16BDDCucumber/hellocucumber/src/test/kotlin/hellocucumber/StepDefinitions.kt
+++ x16BDDCucumber/hellocucumber/src/test/kotlin/hellocucumber/StepDefinitions.kt
@@ -6,7 +6,7 @@ import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
 import junit.framework.Assert.assertEquals
 
-fun isItFriday(today: String) = ""
+fun isItFriday(today: String) = "Nope"
 
 class StepDefinitions {
 
