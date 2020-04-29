6d12d7449b31c1cb5df7154f06b1b80095a4d9d0
Vikram Krishnan <kvikram9885@gmail.com>
2020-04-29 20:24:52 +0800

    Failing implementation

--- x16BDDCucumber/hellocucumber/src/test/kotlin/hellocucumber/StepDefinitions.kt
+++ x16BDDCucumber/hellocucumber/src/test/kotlin/hellocucumber/StepDefinitions.kt
@@ -4,23 +4,40 @@ import io.cucumber.java.PendingException
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
+import junit.framework.Assert.assertEquals
 
+fun isItFriday(today: String) = ""
 
 class StepDefinitions {
 
+    private lateinit var today: String
+    private lateinit var actualAnswer: String
+
+
     @Given("today is Sunday")
-    fun today_is_Sunday() { // Write code here that turns the phrase above into concrete actions
-        throw PendingException()
+    fun today_is_Sunday() {
+
+        today = "Sunday"
+        // Write code here that turns the phrase above into concrete actions
+        //throw PendingException()
     }
 
     @When("I ask whether it's Friday yet")
-    fun i_ask_whether_it_s_Friday_yet() { // Write code here that turns the phrase above into concrete actions
-        throw PendingException()
+    fun i_ask_whether_it_s_Friday_yet() {
+
+        actualAnswer = isItFriday(today)
+
+        // Write code here that turns the phrase above into concrete actions
+        // throw PendingException()
     }
 
     @Then("I should be told {string}")
-    fun i_should_be_told(string: String?) { // Write code here that turns the phrase above into concrete actions
-        throw PendingException()
+    fun i_should_be_told(expectedAnswer: String?) {
+
+        assertEquals(expectedAnswer, actualAnswer)
+
+        // Write code here that turns the phrase above into concrete actions
+        // throw PendingException()
     }
 
 
