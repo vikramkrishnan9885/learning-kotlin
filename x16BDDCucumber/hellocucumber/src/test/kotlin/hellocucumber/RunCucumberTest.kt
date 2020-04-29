930f6d565d434db5f5c3a35ebec875cd6a7aaf6f
Vikram Krishnan <kvikram9885@gmail.com>
2020-04-29 19:59:20 +0800

    Basic classes for BDD

--- x16BDDCucumber/hellocucumber/src/test/kotlin/hellocucumber/RunCucumberTest.kt
+++ x16BDDCucumber/hellocucumber/src/test/kotlin/hellocucumber/RunCucumberTest.kt
@@ -1,2 +1,9 @@
 package hellocucumber
 
+import io.cucumber.junit.CucumberOptions
+import io.cucumber.junit.Cucumber
+import org.junit.runner.RunWith
+
+@RunWith(Cucumber::class)
+@CucumberOptions(plugin = ["pretty"])
+class RunCucumberTest
---
+++ x16BDDCucumber/hellocucumber/src/test/kotlin/hellocucumber/StepDefinitions.kt
@@ -0,0 +1,10 @@
+package hellocucumber
+
+import io.cucumber.java.en.Given
+import io.cucumber.java.en.Then
+import io.cucumber.java.en.When
+import org.junit.Assert.*
+
+
+class StepDefinitions {
+}
