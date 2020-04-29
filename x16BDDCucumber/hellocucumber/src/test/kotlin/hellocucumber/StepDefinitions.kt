41866a21994ee23c7cb148d9e1100f36c2a9a103
Vikram Krishnan <kvikram9885@gmail.com>
2020-04-29 20:21:40 +0800

    Step Definitions

--- x16BDDCucumber/hellocucumber/src/test/kotlin/hellocucumber/StepDefinitions.kt
+++ x16BDDCucumber/hellocucumber/src/test/kotlin/hellocucumber/StepDefinitions.kt
@@ -1,10 +1,27 @@
 package hellocucumber
 
+import io.cucumber.java.PendingException
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
-import org.junit.Assert.*
 
 
 class StepDefinitions {
+
+    @Given("today is Sunday")
+    fun today_is_Sunday() { // Write code here that turns the phrase above into concrete actions
+        throw PendingException()
+    }
+
+    @When("I ask whether it's Friday yet")
+    fun i_ask_whether_it_s_Friday_yet() { // Write code here that turns the phrase above into concrete actions
+        throw PendingException()
+    }
+
+    @Then("I should be told {string}")
+    fun i_should_be_told(string: String?) { // Write code here that turns the phrase above into concrete actions
+        throw PendingException()
+    }
+
+
 }
